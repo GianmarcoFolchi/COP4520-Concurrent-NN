313bf0508bc578e76f0cd707811c6a80f5d8dfd0
Gianmarco Folchi <gmfolc@gmail.com>
2024-03-08 23:59:58 -0500

    final commit

--- FullyConnectedLayer.py
+++ FullyConnectedLayer.py
@@ -1,9 +1,6 @@
 from Layer import Layer
 import numpy as np
 
-# inherit from base class Layer
-
-
 class FCLayer(Layer):
     # input_size = number of input neurons
     # output_size = number of output neurons
@@ -17,13 +14,10 @@ class FCLayer(Layer):
         self.output = np.dot(self.input, self.weights) + self.bias
         return self.output
 
-    # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
     def backward_propagation(self, output_error, learning_rate):
         input_error = np.dot(output_error, self.weights.T)
         weights_error = np.dot(self.input.T, output_error)
-        # dBias = output_error
 
-        # update parameters
         self.weights -= learning_rate * weights_error
         self.bias -= learning_rate * output_error
         return input_error
